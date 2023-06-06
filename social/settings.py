6e7ce53d62aef6d564d26c2a4fe503ca623b0cc8
erik-923 <eblix@elon.edu>
2023-06-05 23:19:23 -0400

    changed settings for production

--- social/settings.py
+++ social/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4i!io71d@s@wfat55ac-!0w0^yjt0nh4c!%_m$%+nciq1%11s8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
