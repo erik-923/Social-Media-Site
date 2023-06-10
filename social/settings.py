c3fc7322cf480ed6209caaf15ae0ed89f839d045
erik-923 <eblix@elon.edu>
2023-06-10 17:21:53 -0400

    again3

--- social/settings.py
+++ social/settings.py
@@ -132,15 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
-)
 
 
 LOGIN_REDIRECT_URL = 'feed'
