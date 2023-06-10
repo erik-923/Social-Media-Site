8189587d7e8ef4ded6764ccd1712d0ed52a5d87c
erik-923 <eblix@elon.edu>
2023-06-10 18:24:04 -0400

    im done

--- social/settings.py
+++ social/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
